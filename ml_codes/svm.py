fd89ea933d7d2bf149333e6f298536afeb3a4644
Oladeji Femi <femidotexe@gmail.com>
2016-09-16 14:22:44 +0100

    Implemented support vector classifier

--- ml_codes/svm.py
+++ ml_codes/svm.py
@@ -1,34 +1,30 @@
-import numpy as np
-from skimage.io import imread
-from skimage.filters import threshold_otsu
-import os
 from sklearn.svm import SVC
-from sklearn.cross_validation import train_test_split, cross_val_score
+from ml_config import MachineLearningConfig
+from ml_validation import AccuracyValidation
 
-training_20X20_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'platenumberchars\\train')
-training_10X20_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'platenumberchars\\new_train')
+config = MachineLearningConfig()
 
-letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+image_data, target_data = config.read_training_data(config.training_data[0])
 
-#each letter has 10 samples
+# sklearn default is 5 but I made this 3
+svc_model = SVC()
 
-image_data = []
-target_data = []
+svc_model.fit(image_data, target_data)
+
+config.save_model(svc_model, 'SVC_model')
 
 
+###############################################
+# for validation and testing purposes
+###############################################
 
-for each_letter in letters:
-    for each in range(10):
-        img_details = imread(training_20X20_dir+'/'+each_letter+'/'+each_letter+'_'+str(each)+'.jpg', as_grey=True)
-        #img_details = imread(training_10X20_dir+'/'+each_letter+'/'+each_letter+'_'+str(each)+'.jpg', as_grey=True)
-        binary_image = img_details < threshold_otsu(img_details)
-        flat_bin_image = binary_image.reshape(-1)
-        image_data.append(flat_bin_image)
-        target_data.append(each_letter)
+validate = AccuracyValidation()
 
-image_data = np.array(image_data)
-target_data = np.array(target_data)
+validate.split_validation(svc_model, image_data, target_data, True)
 
-sv_classifier = SVC(kernel='rbf')
+validate.cross_validation(svc_model, 3, image_data,
+    target_data)
 
-print cross_val_score(sv_classifier, image_data, target_data, cv = 5)
+###############################################
+# end of validation and testing
+###############################################

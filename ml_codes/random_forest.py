75d97215f190c9e56ebf05f2d53c17baf14a25fa
Oladeji Femi <femidotexe@gmail.com>
2016-09-17 19:01:39 +0100

    Implemented random forest classifier

---
+++ ml_codes/random_forest.py
@@ -0,0 +1,30 @@
+from sklearn.ensemble import RandomForestClassifier
+from ml_config import MachineLearningConfig
+from ml_validation import AccuracyValidation
+
+config = MachineLearningConfig()
+
+image_data, target_data = config.read_training_data(config.training_data[0])
+
+# sklearn default is 5 but I made this 3
+rand_forest_classifier = RandomForestClassifier()
+
+rand_forest_classifier.fit(image_data, target_data)
+
+#config.save_model(rand_forest_classifier, 'RandomForest')
+
+
+###############################################
+# for validation and testing purposes
+###############################################
+
+validate = AccuracyValidation()
+
+validate.split_validation(rand_forest_classifier, image_data, target_data, True)
+
+validate.cross_validation(rand_forest_classifier, 3, image_data,
+    target_data)
+
+###############################################
+# end of validation and testing
+###############################################

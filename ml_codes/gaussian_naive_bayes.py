87a6a7bf31d1f73c5da8ea619703e27b68895d2c
Oladeji Femi <femidotexe@gmail.com>
2016-09-17 19:08:06 +0100

    Implemented gaussian naive bayes classifier

---
+++ ml_codes/gaussian_naive_bayes.py
@@ -0,0 +1,30 @@
+from sklearn.naive_bayes import GaussianNB
+from ml_config import MachineLearningConfig
+from ml_validation import AccuracyValidation
+
+config = MachineLearningConfig()
+
+image_data, target_data = config.read_training_data(config.training_data[0])
+
+# sklearn default is 5 but I made this 3
+gaussian_naive_bayes = GaussianNB()
+
+gaussian_naive_bayes.fit(image_data, target_data)
+
+#config.save_model(gaussian_naive_bayes, 'GaussianNB')
+
+
+###############################################
+# for validation and testing purposes
+###############################################
+
+validate = AccuracyValidation()
+
+validate.split_validation(gaussian_naive_bayes, image_data, target_data, True)
+
+validate.cross_validation(gaussian_naive_bayes, 3, image_data,
+    target_data)
+
+###############################################
+# end of validation and testing
+###############################################

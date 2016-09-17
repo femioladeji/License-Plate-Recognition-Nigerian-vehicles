8697eaef6bc24e206696830ee717abd9cbffac00
Oladeji Femi <femidotexe@gmail.com>
2016-09-17 18:56:28 +0100

    Implemented Decision Tree classifier method

---
+++ ml_codes/decision_tree.py
@@ -0,0 +1,30 @@
+from sklearn.tree import DecisionTreeClassifier
+from ml_config import MachineLearningConfig
+from ml_validation import AccuracyValidation
+
+config = MachineLearningConfig()
+
+image_data, target_data = config.read_training_data(config.training_data[0])
+
+# sklearn default is 5 but I made this 3
+tree_classifier = DecisionTreeClassifier()
+
+tree_classifier.fit(image_data, target_data)
+
+#config.save_model(tree_classifier, 'Tree')
+
+
+###############################################
+# for validation and testing purposes
+###############################################
+
+validate = AccuracyValidation()
+
+validate.split_validation(tree_classifier, image_data, target_data, True)
+
+validate.cross_validation(tree_classifier, 3, image_data,
+    target_data)
+
+###############################################
+# end of validation and testing
+###############################################

734e5800001652d0bef1b6909b2fe2aaf9117ef3
Oladeji Femi <femidotexe@gmail.com>
2016-09-19 13:22:26 +0100

    Models can be compared and the probability of the predictions can be seen

--- ml_codes/model_comparison.py
+++ ml_codes/model_comparison.py
@@ -3,6 +3,22 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from ml_config import MachineLearningConfig
 from sklearn.cross_validation import train_test_split
+from operator import itemgetter
+
+config = MachineLearningConfig()
+
+image_data, target_data = config.read_training_data(config.training_data[0])
+
+def top_predictions(predictions):
+    predictions = predictions.reshape(-1).tolist()
+    predictions_label = []
+    for index in range(len(predictions)):
+        predictions_label.append((config.letters[index], predictions[index]))
+
+    predictions_label = sorted(predictions_label, key=itemgetter(1), reverse=True)
+
+    print predictions_label[:5]
+    
 
 sv_model1 = SVC(kernel='linear', probability=True)
 sv_model2 = SVC(kernel='rbf', probability=True)
@@ -10,23 +26,38 @@ n_model = KNeighborsClassifier(n_neighbors=3)
 n_model2 = KNeighborsClassifier(n_neighbors=4)
 
 models = {
-    'linearsvm':sv_model1, 'rbfsvm':sv_model2,
+    'rbfsvm':sv_model2, 'linearsvm':sv_model1,
     '2-neighbor':n_model, '4-neighbor':n_model2
 }
 
-config = MachineLearningConfig()
 
-image_data, target_data = config.read_training_data(config.training_data[0])
 
-img_train, img_test, target_train, target_test = train_test_split(image_data, target_data)
+img_train, img_test, target_train, target_test = train_test_split(image_data,
+    target_data)
+
+prediction2dlist = []
 
 for a_model_name, a_model in models.items():
     print a_model_name
     print '-------------------------------'
     a_model.fit(img_train, target_train)
     prediction = a_model.predict(img_test)
+    prediction2dlist.append(prediction)
     accuracy = (float(np.sum(prediction == target_test)) / len(target_test))
     print str(round(accuracy * 100, 2))+ "% accuracy was recorded"
     #self.print_wrong_predictions(prediction, target_test, img_test, a_model)
     print '-------------------------------'
 
+num_of_test = len(img_test)
+
+for index in range(num_of_test):
+    if(prediction2dlist[1][index] != target_test[index]):
+        print 'Based on LinearSVM'
+        print 'Actual Label : '+ target_test[index]+'. Predicted Label: '+prediction2dlist[1][index]
+        for a_model_name in models:
+            print a_model_name
+            print '-------------------------------'
+            prob_predictions = models[a_model_name].predict_proba(
+                img_test[index].reshape(1, -1))
+            top_predictions(prob_predictions)
+            print '-------------------------------'

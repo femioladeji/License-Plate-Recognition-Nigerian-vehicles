e967e5d6cba6647459c6d5ed0b7cf2d117f227eb
Oladeji Femi <femidotexe@gmail.com>
2016-09-22 19:54:57 +0100

    Automatic creation of db and table

--- dbAspect.py
+++ dbAspect.py
@@ -3,7 +3,12 @@ import pymysql.cursors
 class DBConnection():
     def __init__(self):
         self.connection = pymysql.connect(host='localhost', user='root',
-            password='', db='alpr')
+            password='')
+
+        with self.connection.cursor() as cursor:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alpr")
+
+        self.connection.commit()
 
     def save_alpr(self, license_plate_text, moment):
         """
@@ -16,8 +21,10 @@ class DBConnection():
         """
         try:
             with self.connection.cursor() as cursor:
+                table_sql = "CREATE TABLE IF NOT EXISTS `alpr`.`alpr` ( `id` INT NOT NULL AUTO_INCREMENT , `plate_text` VARCHAR(15) NOT NULL , `moment` VARCHAR(30) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;"
+                cursor.execute(table_sql)
                 #insert record
-                sql = "INSERT INTO alpr (plate_text, moment) VALUES(%s, %s)"
+                sql = "INSERT INTO `alpr`.`alpr` (plate_text, moment) VALUES(%s, %s)"
                 cursor.execute(sql, (license_plate_text, moment))
             self.connection.commit()
 
